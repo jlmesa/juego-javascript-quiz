a5b981972e8a1a2da19ff55109202033666bd504
jlmesa <jolemeag@gmail.com>
2024-04-19 13:38:11 -0500

    Add VITE_URL

--- src/services/questions.ts
+++ src/services/questions.ts
@@ -1,4 +1,4 @@
-const API_URL = import.meta.env.VITE_URL || 'http://localhost:5173';
+const API_URL = import.meta.env.VITE_URL || 'http://localhost:5173/';
 
 export const getAllQuestions = async () => {
   const res = await fetch(`${API_URL}/data.json`);
